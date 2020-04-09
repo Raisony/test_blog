8582e899264d4bdc8236e7829981d1e7ee772352
R <s753242357@gmail.com>
2020-04-08 22:00:47 -0400

    'model_django'

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    path('home/', views.home, name = 'home')
+    path('home/', views.home, name = 'home'),
+
+
+    # url(r'^admin/', include('admin.site.urls'))
+    url(r'', include('blog.urls')),
+
 ]
